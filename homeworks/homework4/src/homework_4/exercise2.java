76191f7145f1b67bf317634dd6f27ec28fdff9c9
fracta <gabriel.c-parent@umontreal.ca>
2014-11-16 00:37:16 -0500

    got the 5 pieces working correctly

--- homeworks/homework4/src/homework_4/exercise2.java
+++ homeworks/homework4/src/homework_4/exercise2.java
@@ -22,7 +22,7 @@ public class exercise2 {
 	 * @param n number of samples to generate (effective samples)
 	 * @return
 	 */
-	public static double[] naive_beta32(int n)
+	public static double[] naive(int n)
 	{
 		// X is U(0, 1)
 		// Y is 16/9 * X
@@ -69,7 +69,7 @@ public class exercise2 {
 	 * @param n
 	 * @return
 	 */
-	public static double[] beta32_3_breakpoints(int n)
+	public static double[] threePieces(int n)
 	{
 		// the optimized breakpoints
 		double x1 = 0.281023;
@@ -103,6 +103,50 @@ public class exercise2 {
 	
 	
 	/**
+	 * Generate points from the Beta(3,2) density with 3 breakpoints as hat function
+	 * As in figure 4.2
+	 * @param n
+	 * @return
+	 */
+	public static double[] fivePieces(int n)
+	{
+		// the optimized breakpoints
+		double x1 = 0.202604;
+		double x2 = 0.381073;
+		double x3 = 0.847968;
+		double x4 = 0.932157;
+		double y1 = beta32(x1);
+		double y2 = beta32(x2);
+		double y3 = beta32(x3);
+		double y4 = beta32(x4);
+		
+		double[] xs = {x1, x2, x3, x4, 1.0};
+		double[] hs = {y1, y2, 16.0 / 9.0, y3, y4};
+		
+		PiecewiseConstantDist dist = new PiecewiseConstantDist(new double[] {0.0, 0.281023, 0.89538, 1.0}, new double[] {0.681365, 16.0/9.0, 1.0064929});
+		
+		
+		RandomStream prng = new MRG32k3a();
+		double[] result = new double[n];
+		int successes = 0;
+		while(successes < n)
+		{
+			double x = dist.inverseF01(prng.nextDouble());
+			double y = prng.nextDouble() * getH(xs, hs, x);
+			// check if the point belongs to the surface of f
+			if(y <= beta32(x))
+			{
+				result[successes] = x;
+				successes += 1;
+			}
+		}
+		return result;	
+		
+	}
+	
+	
+	
+	/**
 	 * Return the time (ms) of execution for the naive sampling algorithm
 	 * @param n
 	 * @return
@@ -110,7 +154,7 @@ public class exercise2 {
 	public static double time_naive(int n)
 	{
 		double startTime = System.currentTimeMillis();
-		double[] generated_numbers = naive_beta32(n);
+		double[] generated_numbers = naive(n);
 		long stopTime = System.currentTimeMillis();
 		double elapsedTime = (stopTime - startTime) / 1000.0;
 		return elapsedTime;
@@ -122,25 +166,41 @@ public class exercise2 {
 	 * @param n
 	 * @return
 	 */
-	public static double time_3_breakpoints(int n)
+	public static double time3Pieces(int n)
 	{
 		double startTime = System.currentTimeMillis();
-		double[] generated_numbers = beta32_3_breakpoints(n);
+		double[] generated_numbers = threePieces(n);
 		double endTime = System.currentTimeMillis();
 		return ((endTime - startTime)/1000.);
 	}
 	
 	
 	/**
+	 * Return the time (ms) of execution for the 5 breakpoints sampling algorithm
+	 * @param n
+	 * @return
+	 */
+	public static double time5Pieces(int n)
+	{
+		double startTime = System.currentTimeMillis();
+		double[] generated_numbers = fivePieces(n);
+		double endTime = System.currentTimeMillis();
+		return ((endTime - startTime)/1000.);
+	}
+	/**
 	 * generate charts to make sure that the accept-reject is working
 	 */
 	public static void test() {
-		double[] a = naive_beta32(100000);
+		double[] a = naive(100000);
 		HistogramChart chart = new HistogramChart("naive method", null, null, a);
 		chart.view(800, 500);
-		double[] b = beta32_3_breakpoints(100000);
+		double[] b = threePieces(100000);
 		HistogramChart chart2 = new HistogramChart("3 breakpoints method", null, null, b);
 		chart2.view(800, 500);
+		
+		double[] c = fivePieces(100000);
+		HistogramChart chart3 = new HistogramChart("5 breakpoints method", null, null, b);
+		chart3.view(800, 500);
 	}
 	
 	/**
@@ -149,24 +209,31 @@ public class exercise2 {
 	 */
 	public static void time(int[] n)
 	{
-		double[][] breakpoints = new double[2][n.length];
+		double[][] fivePieces = new double[2][n.length];
+		double[][] threePieces = new double[2][n.length];
 		double[][] naive = new double[2][n.length];
 		for(int i = 0; i < n.length; i++)
 		{
-			breakpoints[0][i] = n[i];
-			breakpoints[1][i] = time_3_breakpoints(n[i]);
+			threePieces[0][i] = n[i];
+			threePieces[1][i] = time3Pieces(n[i]);
+			fivePieces[0][i] = n[i];
+			fivePieces[1][i] = time5Pieces(n[i]);
 			naive[0][i] = n[i];
 			naive[1][i] = time_naive(n[i]);
 		}
 		
-		//XYLineChart chart = new XYLineChart(null, "X", null, points);
+
 		XYChart naiveTimes = new XYLineChart("naive algorithm running time", "number of samples", "time", naive);
 		naiveTimes.setAutoRange00(true, true); // Axes pass through (0,0)
 		naiveTimes.view(800,500);
 		
-		XYChart breakpointsTimes = new XYLineChart("3 breakpoints algorithm running time", "number of samples", "time", breakpoints);
-		breakpointsTimes.setAutoRange00(true, true); // Axes pass through (0,0)
-		breakpointsTimes.view(800,500);
+		XYChart threePiecesChart = new XYLineChart("3 pieces algorithm running time", "number of samples", "time", threePieces);
+		threePiecesChart.setAutoRange00(true, true); // Axes pass through (0,0)
+		threePiecesChart.view(800,500);
+		
+		XYChart fivePiecesChart = new XYLineChart("5 pieces algorithm running time", "number of samples", "time", fivePieces);
+		fivePiecesChart.setAutoRange00(true, true); // Axes pass through (0,0)
+		fivePiecesChart.view(800,500);
 		
 	}
 	
